39aaa4dce8ee5135a258e59a7859619cd57ff9af
corvoh <corvoh123@gmail.com>
2016-08-18 14:57:40 +0800

    开始写 components/CakeDetail

---
+++ public/src/components/CakeDetail.js
@@ -0,0 +1,21 @@
+/**
+ * Created by hanyile on 16/8/18.
+ */
+
+import React, { Component } from 'react';// eslint-disable-line no-unused-vars
+import Logo from './Logo';
+import Footer from './Footer';
+
+class CakeDetails extends Component {
+  render() {
+    return (
+        <div>
+          <Logo/>
+            233
+          <Footer/>
+        </div>
+    );
+  }
+}
+
+export default CakeDetails;
