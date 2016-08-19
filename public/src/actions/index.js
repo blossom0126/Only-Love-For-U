156abfb764870e19205d7055f828b15cfa227bb4
corvoh <corvoh123@gmail.com>
2016-08-19 15:54:10 +0800

    在 actions/index.js 新增 CAKEDETAIL_LOADED 的 action

--- public/src/actions/index.js
+++ public/src/actions/index.js
@@ -2,3 +2,15 @@ export const addTodo = (text)=> ({
   type: 'ADD_TODO',
   text
 });
+
+export const loadCakeDetail = (data)=> {
+  return {
+    type: CAKEDETAIL_LOADED,
+    data
+  };
+};
+
+
+export const CAKEDETAIL_LOADED = 'CAKEDETAIL_LOADED';
+export const MENUS_LOADED = 'ADD_TODO';
+export const ID_GETTER = 'ID_GETTER';
