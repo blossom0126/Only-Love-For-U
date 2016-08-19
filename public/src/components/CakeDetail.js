c7011ce00b8c6cf9c938a7dfbba443d9f856e1f7
corvoh <corvoh123@gmail.com>
2016-08-19 15:55:07 +0800

    在 cakedetail.js 组件中加入了 mapDispatchToProps 和 connect

--- public/src/components/CakeDetail.js
+++ public/src/components/CakeDetail.js
@@ -2,20 +2,48 @@
  * Created by hanyile on 16/8/18.
  */
 
-import React, { Component } from 'react';// eslint-disable-line no-unused-vars
+import React, { Component } from 'react';// eslint-disable-line no-unused-lets
 import Logo from './Logo';
 import Footer from './Footer';
+import request from 'superagent';
+import {connect} from 'react-redux';
+import {loadCakeDetail} from '../actions/index';
 
-class CakeDetails extends Component {
+class CakeDetailClass extends Component {
+
+    constructor () {
+        super(props);
+    }
+
+    componentDidMount() {
+        request
+            .get(`/cakedetail/${this.props.params.id}`)
+            .end((err, res)=> {
+                this.props.getCakeDetail(res.body);
+            });
+    }
+
   render() {
     return (
         <div>
           <Logo/>
-            233
+          <div>这里展示详情页</div>
           <Footer/>
         </div>
     );
   }
 }
 
-export default CakeDetails;
+let mapStateToProps = (state)=> {
+    return state;
+};
+
+let mapDispatchToProps = (dispatch) => ({
+    getCakeDetail: (data)=> {
+        dispatch(loadCakeDetail(data));
+    }
+});
+
+
+let CakeDetail = connect(mapStateToProps, mapDispatchToProps)(CakeDetailClass);
+export default CakeDetail;
