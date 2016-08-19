824b2d941741f069c312d390c3b89856e82ea08f
corvoh <corvoh123@gmail.com>
2016-08-19 15:51:55 +0800

    新增 cakeDetail.js 容器

---
+++ public/src/container/cakeDetail.js
@@ -0,0 +1,22 @@
+/**
+ * Created by hanyile on 16/8/19.
+ */
+
+import {connect} from 'react-redux';
+import {getItemId} from '../actions/index';
+
+const mapStateToProps = (state) => {
+    return state;
+};
+
+
+const mapDispatchToProps = (dispatch)=> ({
+    getId: (id)=> {
+
+        dispatch(getItemId(id));
+    }
+});
+
+const MenuList = connect(mapStateToProps, mapDispatchToProps);
+
+export default MenuList;
