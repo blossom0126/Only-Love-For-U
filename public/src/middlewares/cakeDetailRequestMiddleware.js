92f71b8275878a015875ecc857917eb75f6389e1
corvoh <corvoh123@gmail.com>
2016-08-19 15:52:38 +0800

    新增 cakeDetailRequestMiddleware.js 中间件

---
+++ public/src/middlewares/cakeDetailRequestMiddleware.js
@@ -0,0 +1,22 @@
+/**
+ * Created by hanyile on 16/8/19.
+ */
+
+import request from 'superagent';
+import {ID_GETTER} from '../actions/index';
+import {loadCakeDetail} from '../actions/index';
+
+const cakeDetailRequestMiddleware = store =>next=>action=> {// eslint-disable-line no-unused-vars
+    switch (action.type) {
+        case  ID_GETTER:
+            request
+                .get(`/cakedetail/${action.id}`)
+                .end((err, res)=> {
+                    next(loadCakeDetail(res.body));
+                });
+            break;
+    }
+    next(action);
+};
+
+export default cakeDetailRequestMiddleware;
