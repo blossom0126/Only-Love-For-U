1f1843ab864ffe73ffade3d43eae4ed43298795d
corvoh <corvoh123@gmail.com>
2016-08-19 15:51:10 +0800

    新增 server/routers/cakeDetail.js

---
+++ server/routers/cakeDetail.js
@@ -0,0 +1,24 @@
+/**
+ * Created by hanyile on 16/8/19.
+ */
+
+import express from 'express';
+import Cake from '../models/Cake';
+
+let router = express.Router();
+
+// //如果没有id的话
+// router.get('/', (req, res)=> {
+//     Cake.find({}, {}, {limit: 1}, (err, data)=> {
+//         res.send(data);
+//     });
+// });
+
+//获取蛋糕详情页的URI并返回结果
+router.get('/:id', (req, res)=> {
+    Cake.findOne({_id: req.params.id}, (err, doc)=> {
+        res.send(doc);
+    });
+});
+
+module.exports = router;
