2b6b77fc949bf5ddeef081210a159cf7aabfada6
corvoh <corvoh123@gmail.com>
2016-08-19 20:29:47 +0800

    韩亦乐 增加了蛋糕页面的跳转链接

--- public/src/components/CakeItem.js
+++ public/src/components/CakeItem.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';// eslint-disable-line no-unused-vars
 
 class CakeItem extends Component {
+
+    click() {
+        this.props.getId(this.props.key);
+    }
+
   render() {
-    let {name, image, style,intro} = this.props;
+    let {name, image, style, intro, id} = this.props;
     return (
         <div>
           <div className="text-center col-xs-6 col-md-3">
-            <a href="#" className="text-center thumbnail">
+            <a href={'/cakeDetail/' + id} className="text-center thumbnail">
               <img src={image} title={intro}  />
             </a>
             <div>{name}</div>
