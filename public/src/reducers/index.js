2d7a2ff6218fd4956fdca24faf34165229267b68
corvoh <corvoh123@gmail.com>
2016-08-22 02:07:50 +0800

    CakeDetailReducer 在 Reducer 层的引用名修改

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,13 +1,12 @@
 import { combineReducers } from 'redux';
 import cake from './cake';
-import cakeDetail from './cakeDetail';
+import CakeDetailReducers from './CakeDetailReducers';
 import user from './users';
 import signup from './signUp';
 
-
 const cakeList = combineReducers({
   cake,
-  cakeDetail,
+  CakeDetailReducers,
   user,
   signup
 });
