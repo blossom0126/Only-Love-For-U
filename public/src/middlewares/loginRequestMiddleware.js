9c35370bf6c3bba3595f0be2816df58dfe454d16
liuru123 <liuru@1457816105qq.com>
2016-08-22 21:08:52 +0800

    liuru --- add login cookies

--- public/src/middlewares/loginRequestMiddleware.js
+++ public/src/middlewares/loginRequestMiddleware.js
@@ -1,21 +1,22 @@
 import request from 'superagent';
+import {browserHistory} from 'react-router';
 
 const loginRequestMiddleware = store =>next=>action=> {// eslint-disable-line no-unused-vars
   switch (action.type) {
   case 'LoginVaild':
-    request.post('/users/login')
+    request.post('/users/logining')
         .type('form')
         .send({
           username: action.text.username,
           password: action.text.password
         })
-        .end((err, res)=> {
-          if (res.body) {
+        .end((err, res)=> {// eslint-disable-line no-unused-vars
+          if (!err) {
+            browserHistory.push('/');
             next({
               type: 'LOGIN_SUCCESS',
               data: '登录成功！'
             });
-
           }
           else {
             next({
@@ -29,4 +30,6 @@ const loginRequestMiddleware = store =>next=>action=> {// eslint-disable-line no
   }
   next(action);
 };
-export default loginRequestMiddleware;
+
+
+module.exports = loginRequestMiddleware;
