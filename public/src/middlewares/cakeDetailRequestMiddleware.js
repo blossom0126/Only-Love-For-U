d670e1c89ae1f53264fd06743429aeb4a45b3079
corvoh <corvoh123@gmail.com>
2016-08-22 15:48:05 +0800

    韩亦乐 在 middlewares/cakeDetailRequestMiddleware.js 中删除了两个没用的 case

--- public/src/middlewares/cakeDetailRequestMiddleware.js
+++ public/src/middlewares/cakeDetailRequestMiddleware.js
@@ -5,37 +5,19 @@
 import request from 'superagent';
 import { LoadCakeDetail } from '../actions/CakeDetailActions';
 
-const cakeDetailRequestMiddleware = store =>next=>action=> {// eslint-disable-line no-unused-vars
+const CakeDetailRequestMiddleware = store =>next=>action=> {// eslint-disable-line no-unused-vars
   switch (action.type) {
-    // case  ID_GETTER:
-    //   console.log('middlewares/cakeDetailRequestMiddelware/ID_GETTER');
-    //   request
-    //       .get(`api/cakedetail/${action.id}`)
-    //       .end((err, res)=> {
-    //         console.log('middlewares/cakeDetailRequestMiddelware/ID_GETTER:.end:res.body:' + res.body);
-    //         next(LoadCakeDetail(res.body));
-    //       });
-    //   break;
-    // case 'CAKEDETAIL_LOADED':
-    //   console.log('middlewares/cakeDetailRequestMiddelware/CAKEDETAIL_LOADED');
-    //   request
-    //       .get(`/api/cakedetail/${action.id}`)
-    //       .end((err, res)=> {
-    //         console.log('middlewares/cakeDetailRequestMiddelware/CAKEDETAIL_LOADED:.end:res.body:' + res.body);
-    //         next(LoadCakeDetail(res.body));
-    //       });
-    //   break;
     case 'CAKEDETAIl_INITIALIZED':
-      console.log('middlewares/cakeDetailRequestMiddelware/CAKEDETAIl_INITIALIZED');
+      console.log('middlewares/CakeDetailRequestMiddelware/CAKEDETAIl_INITIALIZED:action.id:' + action.id);
       request
           .get(`/api/cakedetail/${action.id}`)
           .end((err, res)=> {
+            console.log('middlewares/CakeDetailRequestMiddelware/CAKEDETAIl_INITIALIZED:.end:res.body:' + res.body);
             store.dispatch(LoadCakeDetail(res.body));
-            console.log('middlewares/cakeDetailRequestMiddelware/INIT_CAKEDETAIL:.end:res.body:' + res.body);
           });
       break;
   }
   next(action);
 };
 
-export default cakeDetailRequestMiddleware;
+export default CakeDetailRequestMiddleware;
