d91c36ae057c170c983b2d9cd3e1ed12ca622356
corvoh <corvoh123@gmail.com>
2016-08-22 10:50:27 +0800

    韩亦乐 actions/CakeDetailActions 完成

--- public/src/actions/CakeDetailActions.js
+++ public/src/actions/CakeDetailActions.js
@@ -1,21 +1,22 @@
+// export const CAKEDETAIL_LOADED = 'CAKEDETAIL_LOADED';
+// export const ID_GETTER = 'ID_GETTER';
+
 /**
  * Created by hanyile on 16/8/21.
  */
 
-export const GetCakeID = (id) => {
-  return {
-    type: ID_GETTER,
-    id
-  };
-};
-
 export const LoadCakeDetail = (data)=> {
+  console.log('actions/cakeDetailActions/CAKEDETAIL_LOADED:data:' + data);
   return {
-    type: CAKEDETAIL_LOADED,
+    type: 'CAKEDETAIL_LOADED',
     data
   };
 };
 
-export const CAKEDETAIL_LOADED = 'CAKEDETAIL_LOADED';
-export const ID_GETTER = 'ID_GETTER';
-export const INIT_CAKEDETAIL = 'INIT_MENUDETAIL';
+export const InitializeCakeDetail = (id)=> {
+  console.log('actions/cakeDetailActions/CAKEDETAIl_INITIALIZED:id:' + id);
+  return {
+    type: 'CAKEDETAIl_INITIALIZED',
+    id
+  };
+};
