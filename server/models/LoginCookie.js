a9a83433f3208d18d1435f2a5ea4d86409b8004a
liuru123 <liuru@1457816105qq.com>
2016-08-22 21:09:39 +0800

    liuru --- add loginCookie collection models

--- server/models/LoginCookie.js
+++ server/models/LoginCookie.js
@@ -1,3 +1,7 @@
-/**
- * Created by liuru on 16-8-22.
- */
+import mongoose from 'mongoose';
+const LoginCookie = mongoose.model('LoginCookie',{
+  UUID:String,
+  username:String
+});
+
+module.exports = LoginCookie;
