b9cf46804d0114fcbc724290864664226837c7a5
corvoh <corvoh123@gmail.com>
2016-08-22 10:55:55 +0800

    韩亦乐 删除 reducers/CakeDetailReducers.js 一些没用的 case

--- public/src/reducers/CakeDetailReducers.js
+++ public/src/reducers/CakeDetailReducers.js
@@ -2,19 +2,19 @@
  * Created by hanyile on 16/8/19.
  */
 
-import {ID_GETTER} from '../actions/CakeDetailActions';
-import {CAKEDETAIL_LOADED} from '../actions/CakeDetailActions';
-import {INIT_CAKEDETAIL} from '../actions/CakeDetailActions';
+// import {ID_GETTER} from '../actions/CakeDetailActions';
+// import {CAKEDETAIL_LOADED} from '../actions/CakeDetailActions';
+import {CAKEDETAIl_INITIALIZED} from '../actions/CakeDetailActions';
 
 const cakeDetail = (state = [], action)=> {
+
   switch (action.type) {
-  case CAKEDETAIL_LOADED:
+    // case CAKEDETAIL_LOADED:
+    //   return action.data;
+    // case ID_GETTER:
+    //   return action.data;
+  case 'CAKEDETAIL_LOADED':
     return action.data;
-  case ID_GETTER:
-    return action.data;
-  case INIT_CAKEDETAIL:
-    // return action.data;
-    break;
   }
   return state;
 };
