008fcc5217c47184fede2f90d6545813bc201714
liuru123 <liuru@1457816105qq.com>
2016-08-22 21:07:49 +0800

    liuru ----not  modify cakeItem

--- public/src/components/CakeItem.js
+++ public/src/components/CakeItem.js
@@ -2,9 +2,13 @@ import React, {Component} from 'react';// eslint-disable-line no-unused-vars
 import {Link} from 'react-router';
 class CakeItem extends Component {
 
+  click() {
+    this.props.getId(this.props.key);
+  }
+
   render() {
     let {name, image, style, intro, id} = this.props;
- 
+
     return (
         <div>
           <div className="text-center col-xs-6 col-md-3">
