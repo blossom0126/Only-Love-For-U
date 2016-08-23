b01dc8427361fe1678c4454a3173d72649cd095e
wangpan <lucky.com>
2016-08-23 23:47:03 +0800

    wangpan ----add test of confirmpay

---
+++ spec/routers/confirmpay_spec.js
@@ -0,0 +1,47 @@
+import app from '../../app';
+import request from 'supertest';
+/*global describe,it,done*/
+describe('/api/order/:id', function () {
+  it('should display confirm order information', function () {
+    const id = '12345678';
+    request(app)
+            .get(`/${id}`)
+            .expect([{
+              '_id': '57bc498217c7e4a96ca06d1a',
+              'name': '张三',
+              'tel': '18829290000',
+              'address': '陕西省西安市邮电大学',
+              'more': '请尽快发货',
+              'cakeName': 'Rainbow Paris',
+              'image': 'image/cakelist/08.jpg',
+              'price': 125,
+              'id': '12345678',
+              'isPay': false,
+              '__v': 0
+            }])
+            .end((err) => {
+              if (err) {
+                done.fail(err);
+              } else {
+                done();
+              }
+            });
+  });
+});
+
+describe('/api/order/:id', function () {
+  it('should return statuscode 205', function () {
+    const id = '12345678';
+    request(app)
+            .put(`/${id}`)
+            .query({isPay:true})
+            .expect(205)
+            .end((err) => {
+              if (err) {
+                done.fail(err);
+              } else {
+                done();
+              }
+            });
+  });
+});
